aacd091e814736e0c70be2953dfba2e186b99a9e
ParkSangSun1 <appleasz1028102@gmail.com>
2021-05-06 16:43:56 +0900

    알림 업데이트(알림의 대한 답장)

--- app/src/main/java/com/what/notifications/Tapaction82/MainActivity_82.kt
+++ app/src/main/java/com/what/notifications/Tapaction82/MainActivity_82.kt
@@ -1,9 +1,12 @@
 package com.what.notifications.Tapaction82
 
+import android.app.NotificationManager
 import android.app.RemoteInput
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
+import androidx.core.app.NotificationCompat
 import com.what.notifications.R
 
 class MainActivity_82 : AppCompatActivity() {
@@ -21,6 +24,19 @@ class MainActivity_82 : AppCompatActivity() {
             val inputString = remoteInput.getCharSequence(KEY_REPLY).toString()
             val result_text_view = findViewById<TextView>(R.id.result_text_view)
             result_text_view.text = inputString
+
+            val channelID = "com.what.notifications.SimpleNotificationExample80.channel1"
+            val notificationId = 45
+
+            val repliedNotification = NotificationCompat.Builder(this, channelID)
+                .setSmallIcon(android.R.drawable.ic_dialog_info)
+                .setContentText("Your reply received")
+                .build()
+
+            //알림관리자 인스턴스 가져옴, 시스템 서비스 사용
+            val notificationManager:NotificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            //마지막으로 notificationManager인스턴스를 사용하여 알림
+            notificationManager.notify(notificationId, repliedNotification)
         }
     }
 }
